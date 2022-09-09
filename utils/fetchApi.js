0c79db6d6a3e2716f1bc0792f15adc07b50a14fb
rudinkaca <rudin.kaca@crispybacon.al>
2022-09-09 14:01:57 +0200

    added search location filter

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async (url) => {
+    // const response = await axios.get((url), options);
     const {data} = await axios.get((url), {
         headers: {
             'X-RapidAPI-Key': '0d331c1b96msh319a0ffad52ab25p17f069jsna17885ef318a',
